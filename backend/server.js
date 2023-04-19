c878623eca68ef0d559d918d702f9526208d0b02
choiruladamm <choiruladamm@gmail.com>
2023-04-19 21:24:36 +0700

    Express Router & API Routes

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,19 @@
 const express = require("express");
 require("dotenv").config();
+const workoutRoutes = require("./routes/workouts");
 
 // express app
 const app = express();
 
 // middleware
+app.use(express.json());
 app.use((req, res, next) => {
   console.log(req.path, req.method);
   next();
 });
 
 // routes
-app.get("/", (req, res) => {
-  res.json({ mssg: "Hello workout app" });
-});
+app.use("/api/workouts", workoutRoutes);
 
 // listening for request
 app.listen(process.env.PORT, () =>
