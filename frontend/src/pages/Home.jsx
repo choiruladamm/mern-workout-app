6d6da317a93d88a6ef5bc31a63c12f4cd98535fe
choiruladamm <choiruladamm@gmail.com>
2023-04-21 14:30:47 +0700

    Finished Fetching Data & Styling

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import WorkoutDetails from "../components/WorkoutDetails";
 
 const Home = () => {
   const [workouts, setWorkouts] = useState(null);
@@ -21,9 +22,7 @@ const Home = () => {
     <div className="home">
       <div className="workouts">
         {workouts && workouts.map((workout) => (
-          <p key={workout._id}>
-            {workout.title}
-          </p>
+          <WorkoutDetails key={workout._id} workout={workout} />
         ))}
       </div>
     </div>
