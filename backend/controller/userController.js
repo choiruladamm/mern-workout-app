27082db84150017e7fe844f9a3fda80fa61c30e7
choiruladamm <choiruladamm@gmail.com>
2023-06-14 16:13:42 +0700

    Auth

---
+++ backend/controller/userController.js
@@ -0,0 +1,16 @@
+const User = require("../models/userModel");
+
+// login user
+const loginUser = async (req, res) => {
+  res.json({ mssg: "login user" });
+};
+
+// signup user
+const signupUser = async (req, res) => {
+  res.json({ mssg: "signup user" });
+};
+
+module.exports = {
+  loginUser,
+  signupUser,
+};
